b15c70632c25ae9a4ca4173678322b8e6dead5de
Mahdi <mghorbel1@yahoo.fr>
2025-11-04 17:04:37 +0100

    added more info about blockchain to bank model

--- models/Bank.js
+++ models/Bank.js
@@ -4,16 +4,14 @@ import mongoose from "mongoose";
 const bankSchema = new mongoose.Schema({
     name: { type: String, required: true },
     bicCode: { type: String, required: true, unique: true }, // Bank Identifier Code
-    address: { type: String },
+    address: { type: String, required: false, index: true, lowercase: true, unique: true }, //Wallet address
     registeredAt: { type: Date, default: Date.now },
     isActive: { type: Boolean, default: true },
     blockchain: {
-        status: {
-            type: String,
-            enum: ["pending", "confirmed", "failed"],
-            default: "pending",
-        },
-        transaction: { type: String, default: null },
+        status: { type: String, enum: ["pending", "confirmed", "failed"], default: "pending" },
+        transactionHash: { type: String, default: null, index: true },
+        blockNumber: { type: Number, default: null, index: true },
+        logIndex: { type: Number, default: null },
     },
 });
 
